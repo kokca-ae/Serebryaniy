c423e7b0115cdd2fbb9cceb5df41ff23f2f9e14d
kokca-ae <minik.sarah10@gmail.com>
2020-01-08 14:54:10 +0500

    23
    HomeWork

---
+++ NewCC/NewCC/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace HomeWork1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+
+            int a = 15;
+            int b = 35;
+            string s = "Your salary is: ";
+
+            User u = new User();
+            u.FName = "Evgeniy";
+            u.LName = "Makeev";
+            u.Email = "Makeev@gmail.com";
+            Console.WriteLine(s + a + b);
+           // Console.ReadKey();
+        }
+    }
+
+}
